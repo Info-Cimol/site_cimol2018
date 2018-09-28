89c6304a9965ee0b7f8a61cd8a3e99889a98df67
Everaldo Fernandes <everaldofernandes3110@gmail.com>
2018-09-27 22:06:48 -0300

    alterado!

---
+++ application/modules/site/views/temas/default/index.php
@@ -0,0 +1,9 @@
+<?php
+
+//include_once"aside.php";
+include_once"construcao.php";
+include_once"default.php";
+//include_once"footer.php";
+include_once"formulario.php";
+//include_once"header.php";
+//include_once"main.php";
