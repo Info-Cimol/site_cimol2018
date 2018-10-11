60b03ee611242e6d9e5bb9d4a57e67ff34af08b9
Everaldo Fernandes <everaldofernandes3110@gmail.com>
2018-10-11 20:41:25 -0300

    Teste de commit para inicio

--- application/modules/site/views/temas/default/index.php
+++ application/modules/site/views/temas/default/index.php
@@ -1,7 +1,8 @@
 <?php
 
-//include_once"aside.php";
+
 include_once"construcao.php";
+include_once"aside.php";
 include_once"default.php";
 //include_once"footer.php";
 include_once"formulario_login.php";
